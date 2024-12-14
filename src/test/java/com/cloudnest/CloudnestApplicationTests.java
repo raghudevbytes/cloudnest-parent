47ef8c976b2da6d4b474cc61be14f08b3c69ef57
Raghu <dev.raghu95@gmail.com>
2024-12-14 11:53:41 +0530

    parent pom for all the child cloudnest services

---
+++ src/test/java/com/cloudnest/CloudnestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloudnest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudnestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
